49d9b294bf06323fb3488112cc665be58eff9143
mirzayogy <mirza.yogy@gmail.com>
2021-12-12 20:03:24 +0800

    db_name: praktikum-penggajian-presensi
    create-project laravel/laravel laravel-sigasing-api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/location',function(){
+    return 'Location';
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
