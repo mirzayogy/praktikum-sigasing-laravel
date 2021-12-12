a77121d9bcdd707bf81d8fe27ec66f1770328f75
mirzayogy <mirza.yogy@gmail.com>
2021-12-12 20:50:31 +0800

    Menggunakan LokasiController untuk menampilkan data

--- app/Http/Controllers/LokasiController.php
+++ app/Http/Controllers/LokasiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lokasi;
 use Illuminate\Http\Request;
 
 class LokasiController extends Controller
@@ -13,7 +14,7 @@ class LokasiController extends Controller
      */
     public function index()
     {
-        //
+        return Lokasi::all();
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-use App\Models\Lokasi;
+use App\Http\Controllers\LokasiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/location',function(){
-    return Lokasi::all();
-});
+Route::get('/location',[LokasiController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
