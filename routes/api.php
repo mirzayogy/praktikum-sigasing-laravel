69d59da8a9d113cece8e7812ad95a2a1af764c07
mirzayogy <mirza.yogy@gmail.com>
2021-12-12 20:39:01 +0800

    API GET lokasi

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Lokasi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/location',function(){
-    return 'Location';
+    return Lokasi::all();
 });
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
